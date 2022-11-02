5f8e68ed9220f9c1789f9f3a23283098cfc53b9a
曲木车博 <2791666300@qq.com>
2022-11-02 21:28:30 +0800

    提交

--- src/Routers/AboutBlog/aboutBolg.style.jsx
+++ src/Routers/AboutBlog/aboutBolg.style.jsx
@@ -4,7 +4,8 @@ export const AboutBlogContainer = styled.div`
 	grid-row: 2 / -2;
 	grid-column: 1 / -1;
 	color: white;
-	background-color: black;
+	background-color: #1b1b1d;
+    font-size: 1.5rem;
 	display: flex;
 	flex-direction: column;
 	align-items: center;
--- src/Routers/More/more.style.jsx
+++ src/Routers/More/more.style.jsx
@@ -4,7 +4,8 @@ export const MoreContainer = styled.div`
 	grid-row: 2 / -2;
 	grid-column: 1 / -1;
 	color: white;
-	background-color: black;
+	background-color: #1b1b1d;
+    font-size: 1.5rem;
 	display: flex;
 	align-items: center;
 	justify-content: center;
